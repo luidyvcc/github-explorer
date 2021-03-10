fdea72700b13a9f7765694a8179cd86d94f02a1c
Luidy Vaccaro <luidy@upoutsourcing.com>
2021-03-10 20:37:51 -0300

    module 5 done

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-export function RepositoryItem(props) {
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
   return (
     <li>
       <strong>{props.repository.name}</strong>
@@ -6,7 +14,7 @@ export function RepositoryItem(props) {
 
       <a href={props.repository.html_url}>
         Acessar repositório
-          </a>
+      </a>
     </li>
   );
 }
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,16 @@
+import { useEffect, useState } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
-import { useEffect, useState } from 'react';
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
